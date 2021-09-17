c76079d94051851008b1813fdff6973b3a311225
n-hoffman <62401977+n-hoffman@users.noreply.github.com>
2021-09-17 16:28:04 -0500

    Add files via upload

---
+++ Azure-Immutable-Copy-SQL.ps1
@@ -0,0 +1,42 @@
+#begin variables
+$SQLResourceGroup = "ImmutableTest" #RG of the SQL Server
+$StorageAccountResourceGroup = "ImmutableTest" #Storage Account containing the immutable container
+$SQLServerName = "server1432e" #SQL Server name
+$SQLUsername = "igadmin" #SQL admin account
+$SQLPassword = "Microsoft123" #SQL admin password - can also reference a key vault secret
+$DatabaseNames = "DB1" #DBs to protect, for multiple DBs, use ("DB1","DB2")
+$StorageAccount = "nhtest34sdf34sfsfs"#Storage Account containing the immutable container
+$StorageAccountContainer = "3day-retention"#Immutable Container Name
+#endregion
+#begin processing
+$SecurePassword = ConvertTo-SecureString -String $SQLPassword -AsPlainText -Force
+$SQLCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $SQLUsername, $SecurePassword
+$Date = Get-Date -format MMddyyyy
+$AllDBs = Get-AzSqlDatabase -ServerName $SQLServerName -ResourceGroupName $SQLResourceGroup
+foreach ($D in $AllDBs) {
+    $DBName = $D.DatabaseName
+    if ($DatabaseNames -notcontains $DBName) {
+        Write-Host "$DBName not marked for copy, skipping..."
+        Continue 
+    }
+    $DatabaseName = $D.DatabaseName
+    $StorageAccountKey = (Get-AzStorageAccountKey -Name $StorageAccount -ResourceGroupName $StorageAccountResourceGroup).value[0]
+    $DatabaseCopyName = "$DatabaseName-Copy"
+    $BacpacFilename = "$DatabaseCopyName.bacpac"
+    $BaseStorageUri = "https://" + $StorageAccount + ".blob.core.windows.net"
+    $BacpacURI = "$BaseStorageUri/$StorageAccountContainer/$Date/$BacpacFilename"
+    $BacpacURI = "$BaseStorageUri/test/$Date/$BacpacFilename"
+    Write-Host "Copying" $DatabaseName "to" $DatabaseCopyName
+    New-AzSqlDatabaseCopy -ResourceGroupName $SQLResourceGroup -ServerName $SQLServerName -DatabaseName $DatabaseName -CopyResourceGroupName $SQLResourceGroup -CopyServerName $SQLServerName -CopyDatabaseName $DatabaseCopyName
+    Write-Host "Copy completed"
+    Write-Host "Exporting" $DatabaseCopyName "to" $BacpacUri
+    $ExportRequest = New-AzSqlDatabaseExport -ResourceGroupName $SQLResourceGroup -ServerName $SQLServerName -DatabaseName $DatabaseCopyName -StorageKeyType "StorageAccessKey" -StorageKey $StorageAccountKey -StorageUri $BacpacUri -AdministratorLogin $SQLCredentials.UserName -AdministratorLoginPassword $SQLCredentials.Password
+    $ExportRequest = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $ExportRequest.OperationStatusLink
+    while ($ExportRequest.Status -eq "InProgress")
+    {
+        Start-Sleep -s 10
+        $ExportRequest = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $ExportRequest.OperationStatusLink
+    }
+    Remove-AzSqlDatabase -ResourceGroupName $SQLResourceGroup -ServerName $SQLServerName -DatabaseName $DatabaseCopyName
+}
+#endregion
---
+++ Azure-Immutable-Copy-VM.ps1
@@ -0,0 +1,53 @@
+#begin variables
+$SnapshotResourceGroupName = "ImmutableTest" #Where to store the Snapshots, script will create if not exist
+$Location = "westus2" #Location of the Snapshot RG, Should be same as VMs and Storage Account
+$VMNames = "VM1" #VMs to protect, for multiple VMs, use ("VM1","VM2")
+$StorageAccount = "xxxxxxxxxxxxxxx" #Storage Account containing the immutable container
+$StorageAccountResourceGroup = "ImmutableTest" #Storage Account RG containing the immutable container
+$StorageAccountContainer = "3day-retention" #Immutable Container Name
+#endregion
+#create resource group if not exist
+Try { Get-AzResourceGroup -Name  $SnapshotResourceGroupName -Location $Location -ErrorAction Stop }
+Catch { New-AzResourceGroup -Name  $SnapshotResourceGroupName -Location $Location }
+#endregion
+#begin processing
+$Date = Get-Date -format MMddyyyy
+#collect snapshots
+$AllVMs = Get-AzVM 
+foreach ($v in $AllVMs) {
+    $VMName = $V.Name
+    if ($VMNames -notcontains $VMName) {
+        Write-Host "$VMName not marked for copy, skipping..."
+        Continue 
+    }
+    $VMResourceGroup = $v.ResourceGroupName
+    $VMSnapshotName = "$VMName`_OSDisk"
+    $VM = Get-AzVM -ResourceGroupName $VMResourceGroup -Name $VMName
+    $VMSnapshotCapture = New-AzSnapshotConfig -SourceUri $VM.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy
+    New-AzSnapshot -Snapshot $VMSnapshotCapture -SnapshotName $VMSnapshotName -ResourceGroupName $SnapshotResourceGroupName
+    $VMDataDisks = $vm.StorageProfile.DataDisks
+    foreach ($VMDataDisk in $VMDataDisks) {
+        $DDName = $VMDataDisk.name
+        $DD = Get-Azdisk -DiskName $VMDataDisk.name -ResourceGroupName $VMResourceGroup
+        $DataDiskSnapshotConfig = New-AzSnapshotConfig -SourceUri $DD.Id -CreateOption Copy -Location $Location
+        $DDSnapshotName = "$VMName`_$DDName"
+        New-AzSnapshot -SnapshotName $DDSnapshotName -Snapshot $DataDiskSnapshotConfig -ResourceGroupName $SnapshotResourceGroupName
+    }
+}
+#endregion
+#copy snapshots
+$StorageAccountKey = (Get-AzStorageAccountKey -Name $storageaccount -ResourceGroupName $StorageAccountResourceGroup).value[0]
+$snapshots = Get-AzSnapshot -ResourceGroupName $SnapshotResourceGroupName #| ? { ($_.TimeCreated) -gt ([datetime]::UtcNow.Addhours(-12)) }
+foreach ($Snapshot in $Snapshots) {    
+    Write-Output "Granting $($snapshot.name) access"
+    $snapshotaccess = Grant-AzSnapshotAccess -ResourceGroupName $SnapshotResourceGroupName -SnapshotName $snapshot.Name -DurationInSecond 3600 -Access Read -ErrorAction stop 
+    Write-Output "$($snapshot.name) access granted"
+    $DestStorageContext = New-AzStorageContext –StorageAccountName $storageaccount -StorageAccountKey $StorageAccountKey -ErrorAction stop
+    $vhdname = $Snapshot.Name
+    Write-Output "Begin snapshot: ($($snapshot.name)) copy to $vhdname.vhd"
+    Start-AzStorageBlobCopy -AbsoluteUri $snapshotaccess.AccessSAS -DestContainer $StorageAccountContainer -DestContext $DestStorageContext -DestBlob "$Date/$($vhdname).vhd" -Force -ErrorAction stop
+    Write-Output "snapshot: ($($snapshot.name)) copy to $vhdname.vhd completed"
+    Revoke-AzSnapshotAccess -ResourceGroupName $SnapshotResourceGroupName -SnapshotName $snapshot.Name
+    Remove-AzSnapshot -ResourceGroupName $SnapshotResourceGroupName -SnapshotName $snapshot.Name -force 
+}  
+#endregion
